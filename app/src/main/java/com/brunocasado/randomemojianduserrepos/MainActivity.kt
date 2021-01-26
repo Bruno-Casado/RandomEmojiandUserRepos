55ffafdfb6b027c14b821a6431ad79b88f109db2
Bruno Casado <bruno.lima@cargox.com.br>
2021-01-26 19:36:02 -0300

    Setup databinding on project

--- app/src/main/java/com/brunocasado/randomemojianduserrepos/MainActivity.kt
+++ app/src/main/java/com/brunocasado/randomemojianduserrepos/MainActivity.kt
@@ -3,7 +3,9 @@ package com.brunocasado.randomemojianduserrepos
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.brunocasado.randomemojianduserrepos.databinding.ActivityMainBinding
 import dagger.android.AndroidInjection
 import javax.inject.Inject
 
@@ -12,13 +14,17 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
-    val mainActivityViewModel: MainActivityViewModel by viewModels {
+    private val mainActivityViewModel: MainActivityViewModel by viewModels {
         viewModelFactory
     }
+
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = mainActivityViewModel
+        binding.lifecycleOwner = this
     }
 }
--- app/src/main/java/com/brunocasado/randomemojianduserrepos/MainActivityViewModel.kt
+++ app/src/main/java/com/brunocasado/randomemojianduserrepos/MainActivityViewModel.kt
@@ -1,7 +1,13 @@
 package com.brunocasado.randomemojianduserrepos
 
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
 class MainActivityViewModel @Inject constructor() : ViewModel() {
+
+    fun getEmoji() = View.OnClickListener {
+        Log.d("Teste", "Clicou")
+    }
 }
