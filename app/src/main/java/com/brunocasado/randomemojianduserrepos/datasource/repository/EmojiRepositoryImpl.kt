d5853578d7eb96796fa25b1c8a649a90bbe52077
Bruno Casado <bruno.lima@cargox.com.br>
2021-02-02 19:34:47 -0300

    Include NetworkInfo on EmojiRepositoryImpl

--- app/src/main/java/com/brunocasado/randomemojianduserrepos/datasource/repository/EmojiRepositoryImpl.kt
+++ app/src/main/java/com/brunocasado/randomemojianduserrepos/datasource/repository/EmojiRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.brunocasado.randomemojianduserrepos.datasource.repository
 import com.brunocasado.randomemojianduserrepos.EmojiFailure
 import com.brunocasado.randomemojianduserrepos.core.Either
 import com.brunocasado.randomemojianduserrepos.core.exception.Failure
+import com.brunocasado.randomemojianduserrepos.core.network.NetworkInfo
 import com.brunocasado.randomemojianduserrepos.datasource.entity.Emoji
 import com.brunocasado.randomemojianduserrepos.datasource.network.ApiService
 import kotlinx.coroutines.Dispatchers
@@ -10,17 +11,22 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class EmojiRepositoryImpl @Inject constructor(
-    private val apiService: ApiService
+    private val apiService: ApiService,
+    private val networkInfo: NetworkInfo
 ) : EmojiRepository {
 
     override suspend fun getEmojiList(): Either<Failure, List<Emoji>> {
-        return withContext(Dispatchers.IO) {
-            try {
-                val response = apiService.getEmojiList()
-                Either.Right(response.emojiList)
-            } catch (ex: Exception) {
-                Either.Left(EmojiFailure.ListNotAvailable)
+        return if (networkInfo.isNetworkAvailable()) {
+            withContext(Dispatchers.IO) {
+                try {
+                    val response = apiService.getEmojiList()
+                    Either.Right(response.emojiList)
+                } catch (ex: Exception) {
+                    Either.Left(EmojiFailure.ListNotAvailable)
+                }
             }
+        } else {
+            Either.Left(Failure.NetworkConnection)
         }
     }
 }
--- app/src/main/java/com/brunocasado/randomemojianduserrepos/di/module/RepositoryModule.kt
+++ app/src/main/java/com/brunocasado/randomemojianduserrepos/di/module/RepositoryModule.kt
@@ -1,5 +1,7 @@
 package com.brunocasado.randomemojianduserrepos.di.module
 
+import android.content.Context
+import com.brunocasado.randomemojianduserrepos.core.network.NetworkInfoImpl
 import com.brunocasado.randomemojianduserrepos.datasource.network.ApiService
 import com.brunocasado.randomemojianduserrepos.datasource.repository.EmojiRepository
 import com.brunocasado.randomemojianduserrepos.datasource.repository.EmojiRepositoryImpl
@@ -9,7 +11,7 @@ import dagger.Provides
 @Module
 class RepositoryModule {
     @Provides
-    internal fun providesEmojiRepository(apiService: ApiService): EmojiRepository {
-        return EmojiRepositoryImpl(apiService)
+    internal fun providesEmojiRepository(apiService: ApiService, context: Context): EmojiRepository {
+        return EmojiRepositoryImpl(apiService, NetworkInfoImpl(context))
     }
 }
