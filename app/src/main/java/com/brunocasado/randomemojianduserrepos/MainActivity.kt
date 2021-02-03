dbafcfbd8012e7cbd2f9dc246fe14fc307962e83
Bruno Casado <bruno.lima@cargox.com.br>
2021-02-03 20:36:54 -0300

    Change Get Emoji button into Random Emoji

--- app/src/main/java/com/brunocasado/randomemojianduserrepos/MainActivity.kt
+++ app/src/main/java/com/brunocasado/randomemojianduserrepos/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.brunocasado.randomemojianduserrepos
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.brunocasado.randomemojianduserrepos.databinding.ActivityMainBinding
+import com.bumptech.glide.Glide
 import dagger.android.AndroidInjection
 import javax.inject.Inject
 
@@ -23,8 +27,66 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
+        initBinding()
+        initViewModel()
+    }
+
+    private fun initBinding() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.viewModel = mainActivityViewModel
         binding.lifecycleOwner = this
     }
+
+    private fun initViewModel() {
+        initViewModelActions()
+        observeEmojis()
+    }
+
+    private fun initViewModelActions() {
+        mainActivityViewModel.showNetworkConnectionError = {
+            showToast(getString(R.string.network_connection_error_message))
+        }
+        mainActivityViewModel.showPersistenceError = {
+            showToast(getString(R.string.persistence_error_message))
+        }
+        mainActivityViewModel.showServerError = {
+            showToast(getString(R.string.server_error_message))
+        }
+        mainActivityViewModel.showSuccessMessage = {
+            showToast(getString(R.string.success_message))
+        }
+        mainActivityViewModel.showLoadEmojiIntoImageViewError = {
+            showToast(getString(R.string.random_emoji_error_message))
+        }
+        mainActivityViewModel.loadUrlIntoImageView = { emoji ->
+            loadImageInto(emoji.url, binding.randomEmojiImageView)
+            setContentDescriptionOnImageView(emoji.name, binding.randomEmojiImageView)
+        }
+    }
+
+    private fun showToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show()
+    }
+
+    private fun setContentDescriptionOnImageView(contentDescription: String, imageView: ImageView) {
+        imageView.contentDescription = contentDescription
+    }
+
+    private fun loadImageInto(url: String, imageView: ImageView) {
+        Glide.with(this).load(url).into(imageView)
+    }
+
+    private fun observeEmojis() {
+        mainActivityViewModel.emojis.observe(this, {
+            if (it.isNullOrEmpty()) {
+                binding.getEmojiButton.text = getText(R.string.get_emoji_button)
+            } else {
+                binding.getEmojiButton.text = getText(R.string.get_random_emoji_button)
+                binding.getEmojiButton.setOnClickListener {
+                    mainActivityViewModel.showRandomEmoji()
+                }
+            }
+        })
+    }
+
 }
--- app/src/main/java/com/brunocasado/randomemojianduserrepos/MainActivityViewModel.kt
+++ app/src/main/java/com/brunocasado/randomemojianduserrepos/MainActivityViewModel.kt
@@ -1,13 +1,76 @@
 package com.brunocasado.randomemojianduserrepos
 
-import android.util.Log
-import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.brunocasado.randomemojianduserrepos.core.Either
+import com.brunocasado.randomemojianduserrepos.core.exception.Failure
+import com.brunocasado.randomemojianduserrepos.datasource.entity.Emoji
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MainActivityViewModel @Inject constructor() : ViewModel() {
+class MainActivityViewModel @Inject constructor(
+    private val emojiListUseCase: EmojiListUseCase
+) : ViewModel() {
 
-    fun getEmoji() = View.OnClickListener {
-        Log.d("Teste", "Clicou")
+    lateinit var showNetworkConnectionError: () -> Unit
+    lateinit var showPersistenceError: () -> Unit
+    lateinit var showServerError: () -> Unit
+    lateinit var showSuccessMessage: () -> Unit
+    lateinit var loadUrlIntoImageView: (Emoji) -> Unit
+    lateinit var showLoadEmojiIntoImageViewError: () -> Unit
+
+    private val _emojis = MutableLiveData<List<Emoji>>().apply { value = emptyList() }
+    val emojis: LiveData<List<Emoji>> = _emojis
+
+    private val _isLoading = MutableLiveData<Boolean>().apply { value = false }
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    init {
+        getEmoji()
+    }
+
+    fun getEmoji() {
+        _isLoading.value = true
+        viewModelScope.launch {
+            val emojiListFuture = async {
+                emojiListUseCase.invoke()
+            }
+
+            val emojiListFutureResult: Either<Failure, List<Emoji>> = emojiListFuture.await()
+            when (emojiListFutureResult) {
+                is Either.Right -> {
+                    handleGetEmojiListSuccess(emojiListFutureResult.b)
+                }
+                is Either.Left -> {
+                    handleGetEmojiListError(emojiListFutureResult.a)
+                }
+            }
+            _isLoading.value = false
+        }
+    }
+
+    fun showRandomEmoji() {
+        val randomEmoji = emojis.value?.random()
+        randomEmoji?.let {
+            loadUrlIntoImageView(it)
+        } ?: showLoadEmojiIntoImageViewError()
+    }
+
+    private fun handleGetEmojiListSuccess(emojiList: List<Emoji>) {
+        _emojis.value = emojiList
+        showSuccessMessage()
+    }
+
+    private fun handleGetEmojiListError(failure: Failure) {
+        when (failure) {
+            is Failure.NetworkConnection -> showNetworkConnectionError()
+            is EmojiFailure.SaveEmojiListPersistenceError -> showPersistenceError()
+            is EmojiFailure.GetEmojiListPersistenceError -> showPersistenceError()
+            is EmojiFailure.ServerError -> showServerError()
+            else -> showServerError()
+        }
     }
 }
