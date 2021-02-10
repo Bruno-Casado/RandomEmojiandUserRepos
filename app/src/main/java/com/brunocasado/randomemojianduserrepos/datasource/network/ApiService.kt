47c4e5a1bb2e3910f555137da7828e7e6b57c273
Bruno Casado <bruno.lima@cargox.com.br>
2021-02-09 22:41:32 -0300

    Create getGoogleRepos api call

--- app/src/main/java/com/brunocasado/randomemojianduserrepos/datasource/network/ApiService.kt
+++ app/src/main/java/com/brunocasado/randomemojianduserrepos/datasource/network/ApiService.kt
@@ -1,6 +1,7 @@
 package com.brunocasado.randomemojianduserrepos.datasource.network
 
 import com.brunocasado.randomemojianduserrepos.datasource.entity.EmojiResponse
+import com.brunocasado.randomemojianduserrepos.datasource.entity.Repo
 import com.brunocasado.randomemojianduserrepos.datasource.entity.User
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -11,6 +12,7 @@ interface ApiService {
         private const val EMOJIS = "emojis"
         private const val USER = "user"
         private const val USERS = "users/{$USER}"
+        private const val GOOGLE_REPOS = "users/google/repos"
         const val API_URL = "https://api.github.com/"
     }
 
@@ -21,4 +23,7 @@ interface ApiService {
     suspend fun getUser(
         @Path(USER) user: String
     ): User
+
+    @GET(GOOGLE_REPOS)
+    suspend fun getGoogleRepos(): List<Repo>
 }
