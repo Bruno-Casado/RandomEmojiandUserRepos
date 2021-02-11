19a2b3be5dc2485f554e19c2e9667431e8dfab77
Bruno Casado <bruno.lima@cargox.com.br>
2021-02-10 22:48:50 -0300

    Create more tests for MainActivityViewModel related to getUser

--- app/src/test/java/com/brunocasado/randomemojianduserrepos/viewmodel/MainActivityViewModelTest.kt
+++ app/src/test/java/com/brunocasado/randomemojianduserrepos/viewmodel/MainActivityViewModelTest.kt
@@ -8,9 +8,11 @@ import com.brunocasado.randomemojianduserrepos.MainActivityViewModel
 import com.brunocasado.randomemojianduserrepos.core.Either
 import com.brunocasado.randomemojianduserrepos.core.exception.Failure
 import com.brunocasado.randomemojianduserrepos.datasource.entity.Emoji
+import com.brunocasado.randomemojianduserrepos.datasource.entity.User
 import com.brunocasado.randomemojianduserrepos.helper.MainCoroutineRule
 import com.brunocasado.randomemojianduserrepos.helper.mock
 import com.brunocasado.randomemojianduserrepos.helper.whenever
+import com.brunocasado.randomemojianduserrepos.useravatar.UserFailure
 import com.brunocasado.randomemojianduserrepos.useravatar.UserUseCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
@@ -39,11 +41,13 @@ class MainActivityViewModelTest {
     private lateinit var userUserCase: UserUseCase
 
     private lateinit var viewModel: MainActivityViewModel
+    private lateinit var user: User
 
     @Before
     fun setup() {
         emojiListUseCase = mock()
         userUserCase = mock()
+        user = User("Bruno-Casado", 8345519, "https://avatars.githubusercontent.com/u/8345519?v=4")
         mainCoroutineRule.pauseDispatcher()
         viewModel = MainActivityViewModel(emojiListUseCase, userUserCase)
     }
@@ -182,5 +186,94 @@ class MainActivityViewModelTest {
         assert(viewModel.emojis.value?.isEmpty() ?: false)
 
         viewModel.emojis.removeObserver(emojis)
+    }
+
+    @Test
+    fun `When getUser is invoked without network connection and has no cache then showNetworkConnectionError must be invoked`() {
+        val isLoading = mock<Observer<Boolean>>()
+        viewModel.isLoading.observeForever(isLoading)
+        viewModel.showNetworkConnectionError = mock()
+        viewModel.userLogin = user.login
+
+        runBlocking {
+            whenever(userUserCase.invoke(user.login)).thenReturn(Either.Left(Failure.NetworkConnection))
+
+            viewModel.getUser()
+
+            assert(viewModel.isLoading.value == true)
+        }
+
+        mainCoroutineRule.resumeDispatcher()
+
+        verify(viewModel.showNetworkConnectionError).invoke()
+        assert(viewModel.isLoading.value == false)
+
+        viewModel.isLoading.removeObserver(isLoading)
+    }
+
+    @Test
+    fun `When getUser is invoked and persistence error occurs then showPersistenceError must be invoked`() {
+        val isLoading = mock<Observer<Boolean>>()
+        viewModel.isLoading.observeForever(isLoading)
+        viewModel.showPersistenceError = mock()
+        viewModel.userLogin = user.login
+
+        runBlocking {
+            whenever(userUserCase.invoke(user.login)).thenReturn(Either.Left(UserFailure.SaveUserPersistenceError))
+            viewModel.getUser()
+            assert(viewModel.isLoading.value == true)
+        }
+
+        mainCoroutineRule.resumeDispatcher()
+
+        verify(viewModel.showPersistenceError).invoke()
+        assert(viewModel.isLoading.value == false)
+
+        viewModel.isLoading.removeObserver(isLoading)
+    }
+
+    @Test
+    fun `When getUser is invoked and server error occurs then showServerError must be invoked`() {
+        val isLoading = mock<Observer<Boolean>>()
+        viewModel.isLoading.observeForever(isLoading)
+        viewModel.showServerError = mock()
+        viewModel.userLogin = user.login
+
+        runBlocking {
+            whenever(userUserCase.invoke(user.login)).thenReturn(Either.Left(Failure.ServerError))
+
+            viewModel.getUser()
+
+            assert(viewModel.isLoading.value == true)
+        }
+
+        mainCoroutineRule.resumeDispatcher()
+
+        verify(viewModel.showServerError).invoke()
+        assert(viewModel.isLoading.value == false)
+
+        viewModel.isLoading.removeObserver(isLoading)
+    }
+
+    @Test
+    fun `When getUser is invoked and returns data then displayAvatarOnEmojiImageHolder should be invoked`() {
+        val isLoading = mock<Observer<Boolean>>()
+        viewModel.isLoading.observeForever(isLoading)
+        viewModel.displayAvatarOnEmojiImageHolder = mock()
+        viewModel.userLogin = user.login
+
+        runBlocking {
+            whenever(userUserCase.invoke(user.login)).thenReturn(Either.Right(user))
+
+            viewModel.getUser()
+
+            assert(viewModel.isLoading.value == true)
+        }
+
+        mainCoroutineRule.resumeDispatcher()
+
+        assert(viewModel.isLoading.value == false)
+        verify(viewModel.displayAvatarOnEmojiImageHolder).invoke(user.avatarUrl)
+        viewModel.isLoading.removeObserver(isLoading)
     }
 }
